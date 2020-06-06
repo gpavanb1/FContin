33d0f2223cf87c264654574a322a6af1e2815209
Pavan BG <gpbabc@yahoo.com>
2020-06-06 20:26:12 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='FContin',
+      version='0.1',
+      description='Numerical continuation using just the function',
+      url='https://github.com/gpavanb1/FContin',
+      author='gpavanb1',
+      author_email='gpavanb@gmail.com',
+      license='MIT',
+      packages=['fcontin'],
+      install_requires=["numpy",
+      "scipy",
+      "jax",
+      "jaxlib",
+      "numdifftools"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      zip_safe=False)
